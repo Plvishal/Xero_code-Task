c895f3166ec8096b000b3ecea90ea67dda761dea
Vishal <vishalpal1501@gmail.com>
2024-05-31 15:35:04 +0530

    appwrite setup done for register user login user and logout

---
+++ src/lib/appwrite.js
@@ -0,0 +1,10 @@
+import { Client, Account } from 'appwrite';
+
+const client = new Client();
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_URL) // Your Appwrite endpoint
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID); // Your project ID
+
+const account = new Account(client);
+
+export { client, account };
---
+++ src/lib/auth.js
@@ -0,0 +1,38 @@
+import { account } from './appwrite';
+
+export async function register(email, password, name) {
+  try {
+    const response = await account.create('unique()', email, password, name);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error('Error registering user:', error);
+    throw error;
+  }
+}
+
+export async function login(email, password) {
+  try {
+    const response = await account.createSession(email, password);
+    return response;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    throw error;
+  }
+}
+
+export async function logout() {
+  try {
+    return await account.deleteSession("current")
+  } catch (error) {
+    console.log('Logout error :' + error.message);
+  }
+}
+
+export function loginWithGoogle() {
+  account.createOAuth2Session('google');
+}
+
+export function loginWithGitHub() {
+  account.createOAuth2Session('github');
+}
